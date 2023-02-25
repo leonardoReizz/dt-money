02920c78e069d0c2e16b80d2d71b48a141cac12d
Leonardo Reis <amarantee.reis@gmail.com>
2023-02-24 23:21:46 -0300

    fix: fetch transactions with api

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,31 +1,38 @@
-import { MagnifyingGlass } from "phosphor-react";
-import { useForm } from "react-hook-form";
-import { SearchFormContainer } from "./styles";
-import * as z from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { SearchFormContainer } from './styles'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 
 const searchFormSchema = z.object({
-  query: z.string()
+  query: z.string(),
 })
 
-type SearchFormInputs = z.infer<typeof searchFormSchema>;
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
-    resolver: zodResolver(searchFormSchema)
-  });
+  const { fetchTransactions } = useContext(TransactionsContext)
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    fetchTransactions(data.query)
   }
 
   return (
     <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
-      <input 
-        type="text" 
-        placeholder="Busque por transações"  
-        {...register('query')} 
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
       />
       <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
